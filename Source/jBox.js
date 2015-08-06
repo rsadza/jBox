75dfee6258fa0577a5de2596f406be94e7cc7486
Roel Sadza <rsadza@gmail.com>
2015-08-06 23:20:14 +0200

    Update jBox.js

--- Source/jBox.js
+++ Source/jBox.js
@@ -20,12 +20,12 @@ demos: http://stephanwagner.me/jBox/demos
 	// AMD. Register as an anonymous module
     if (typeof define === 'function' && define.amd) {
         define(['jQuery'], function (jQuery) {
-            return (root.jBox = factory(jquery));
+            return (root.jBox = factory(jQuery));
         });
     
     // Node. Does not work with strict CommonJS, but only CommonJS-like enviroments that support module.exports, like Node
     } else if (typeof exports === 'object') {
-        module.exports = factory(require('jQuery'));
+        module.exports = factory(require('jquery'));
     
     // Browser globals
     } else {
